9640e36dacbc473430b3de3d816bdfa70a5b3cc3
chiemezie1 <chiemezieagbo1@gmail.com>
2023-07-29 20:42:22 +0100

    setup

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const multer = require('multer');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const path = require('path');
+const {fileURLToPath} = require('url');
+
+// Load configuration
+
+const filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(filename);
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({
+    policy: "cross-origin"
+}));
+app.use(morgan("common"))
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
