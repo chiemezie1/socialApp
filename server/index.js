b51293af14b9a518efdd889eeaf7687daca68b14
chiemezie1 <chiemezieagbo1@gmail.com>
2023-07-31 20:00:22 +0100

    .env

--- server/index.js
+++ server/index.js
@@ -36,3 +36,11 @@ const fileStorageEngin = multer.diskStorage({
 });
 
 const upload = multer({storage: fileStorageEngin});
+
+// MongoDB setup
+const port = process.env.PORT || 5000;
+mongoose.connect(process.env.MONDODB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => {
+    app.listen(port, () => console.log(`Server running on port ${port}`));
+})
+.catch((err) => console.log(err));
