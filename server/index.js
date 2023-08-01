0f7c014d1715364d10ed2dcca5820d5f98b3172f
chiemezie1 <chiemezieagbo1@gmail.com>
2023-08-01 21:48:35 +0100

    updated thr index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,11 @@ import path  from 'path';
 import {fileURLToPath} from 'url';
 import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js'
+import postRoutes from './routes/posts.js';
 import {register} from './controllers/auth.js';
+import {createPost} from './controllers/posts.js';
+import { verifyToken } from './middleware/auth.js';
+import { create } from 'domain';
 
 
 // Load configuration
@@ -43,6 +47,8 @@ const upload = multer({storage: fileStorageEngin});
 
 // Routes with authentication
 app.post("auth/register", upload.single('picture'), register);
+app.post('/posts', verifyToken, upload.single('picture'), createPost);
+
 
 // Routes without authentication
 app.use('/auth', authRoutes);
