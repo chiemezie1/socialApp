3d78e634d7e960a5978839fc0ce9bb9027f454b9
chiemezie1 <chiemezieagbo1@gmail.com>
2023-08-02 16:08:49 +0100

    liked function

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -26,3 +26,49 @@ export const createPost = async (req, res) => {
         res.send(500).json({error: err.message})
     }
 }
+
+
+////////////////--Read----/////
+
+export const getFeedPost = async (req, res) => {
+    try{
+        const post = await Post.find();
+        res.status(200).json(post);
+
+    } catch (err) {
+        res.send(500).json({error: err.message})
+    }
+};
+
+export const getUserPosts = async (req, res) => {
+    try{
+        const { userId } = req.params;
+        const userPost = await Post.find({userId});
+        res.status(200).json(userPost);
+
+    } catch (err) {
+        res.send(500).json({error: err.message});
+    }
+};
+
+
+////////////////--Update----/////
+
+export const likePost = async (req, res) => {
+    try{
+        const { id } = req.params;
+        const { userId } = req.body;
+        const post = await Post.findById(id);
+        const isLiked = post.likes.get(userId);
+        
+        if(isLiked){
+            post.likes.delete(userId);
+        } else {
+            post.likes.set(userId, true);
+        }
+        await post.save();
+
+    } catch (err) {
+        res.send(500).json({error: err.message});
+    }
+}
